3cc10ff8e066f361562082dfc0f4305642d7983a
xyc0833 <2031362123@qq.com>
2026-03-04 10:47:32 +0800

    垃圾回收 最终判定

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("123123");
+        System.out.println("123131233332");
     }
 }
