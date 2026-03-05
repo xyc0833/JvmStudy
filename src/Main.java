844f5c1cdc1778edb92e130186fa09c96e5df968
xyc0833 <2031362123@qq.com>
2026-03-05 09:39:52 +0800

    可重入锁

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,19 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Lock testLock = new ReentrantLock();
-        Lock test02 = new ReentrantLock();
-        new Thread(() -> {
-            testLock.lock();
-            try {
-                System.out.println("等待是否未超时："+testLock.newCondition().await(1, TimeUnit.SECONDS));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            testLock.unlock();
+        //可重入锁 ： 可以反复进行加锁操作
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        lock.lock();
+        new Thread(()->{
+            System.out.println("线程2想要获取锁");
+            lock.lock();
+            System.out.println("线程2成功获取到锁");
         }).start();
-
-        System.out.println("60秒 = "+TimeUnit.SECONDS.toMinutes(60) +"分钟");
-        System.out.println("365天 = "+TimeUnit.DAYS.toSeconds(365) +" 秒");
+        lock.unlock();
+        System.out.println("线程1释放了一次锁");
+        TimeUnit.SECONDS.sleep(1);
+        lock.unlock();
+        System.out.println("线程1再次释放了一次锁");  //释放两次后其他线程才能加锁
     }
 }
