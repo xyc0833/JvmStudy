4e1cf5208c9abf68442cd59873458a913f2e05ad
xyc0833 <2031362123@qq.com>
2026-03-06 15:37:21 +0800

    线程池的使用

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,37 @@ import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //通过多线程编程，来模拟一个餐厅的2个厨师和3个顾客，假设厨师炒出一个菜的时间为3秒，顾客吃掉菜品的时间为4秒，窗口上只能放一个菜。
-        //通过阻塞队列实现生产者消费者模式
-        BlockingQueue<Object> queue = new ArrayBlockingQueue<>(1);
-        //生产者
-        Runnable supplier = () -> {
-            while(true){
+        //手动创建线程池
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2,4,//2个核心线程，最大线程数为4个
+        3,TimeUnit.SECONDS,//最大空闲时间为3秒钟
+                new SynchronousQueue<>(),//有界带缓冲阻塞队列
+                (r, executor1) -> {   //比如这里我们也来实现一个就在当前线程执行的策略
+                    System.out.println("哎呀，线程池和等待队列都满了，你自己耗子尾汁吧");
+                    r.run();   //直接运行
+                });
+
+        //开始6个任务
+        for(int i=0;i<6;i++){
+            int finalI = i;
+            executor.execute( () -> {
                 try{
-                    String name = Thread.currentThread().getName();
-                    System.err.println(time() + "生产者" + name + " 正在准备餐品...");
-                    TimeUnit.SECONDS.sleep(3);
-                    System.err.println(time() + "生产者" + name + " 已经出餐");
-                    queue.put(new Object());
+                    System.out.println(Thread.currentThread().getName() + " 开始执行 " + finalI);
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.println(Thread.currentThread().getName()+" 已结束！（"+finalI);
                 }catch (InterruptedException e){
                     e.printStackTrace();
-                    break;
                 }
-            }
-        };
-        Runnable consumer = () -> {
-            while (true){
-                try {
-                    String name = Thread.currentThread().getName();
-                    System.out.println(time()+"消费者 "+name+" 正在等待出餐...");
-                    queue.take();
-                    System.out.println(time()+"消费者 "+name+" 取到了餐品。");
-                    TimeUnit.SECONDS.sleep(4);
-                    System.out.println(time()+"消费者 "+name+" 已经将饭菜吃完了！");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        };
-        for(int i=0;i<2;i++){
-            new Thread(supplier,"厨师" + i).start();
+            });
         }
-        for(int i=0;i<3;i++){
-            new Thread(consumer,"顾客" + i).start();
-        }
-    }
+        TimeUnit.SECONDS.sleep(1);    //看看当前线程池中的线程数量
+        System.out.println("线程池中的线程数量 " + executor.getPoolSize());
+        TimeUnit.SECONDS.sleep(5);     //等到超过空闲时间
+        System.out.println("线程池中的线程数量 " + executor.getPoolSize());
 
-    public static String time(){
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-        return "[" + format.format(new Date()) + "]";
+        //使用完线程池记得关闭，不然程序不会结束，它会取消所有等待中的任务以及试图中断正在执行的任务，
+        // 关闭后，无法再提交任务，一律拒绝
+        executor.shutdownNow();
+
+        //executor.shutdown();     同样可以关闭，但是会执行完等待队列中的任务再关闭
     }
 }
