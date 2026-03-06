0903a810fb456a3a56b4908f1b67ea9705f8871d
xyc0833 <2031362123@qq.com>
2026-03-06 10:14:32 +0800

    ConcurrentHashMap

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //线程安全的并发容器
-        List<String> list = new CopyOnWriteArrayList<>();
+        Map<Integer, String> map = new ConcurrentHashMap<>();
         for (int i = 0; i < 100; i++) {
             int finalI = i;
             new Thread(() -> {
                 for (int j = 0; j < 100; j++)
-                    list.add("lbwnb");
+                    map.put(finalI * 100 + j, "lbwnb");
             }).start();
         }
-        TimeUnit.SECONDS.sleep(2);
-        System.out.println(list.size());
-        System.out.println(list.toString());
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(map.size());
     }
 }
